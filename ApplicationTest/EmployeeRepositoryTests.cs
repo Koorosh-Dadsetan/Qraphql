8f30de8fddfcd0d1108edc51c89faade3686133e
Koorosh Dadsetan <kooroshdadsetan@outlook.com>
2024-04-13 01:26:50 +0330

    14030124_3

--- ApplicationTest/EmployeeRepositoryTests.cs
+++ ApplicationTest/EmployeeRepositoryTests.cs
@@ -1,31 +1,50 @@
 using Application.DTO;
 using Application.Repository.Employee;
 using Domain.Context;
-using Moq;
+using Domain.Models;
 
 namespace ApplicationTest
 {
     public class EmployeeRepositoryTests
     {
-        public readonly TestDbContext _db;
-        public EmployeeRepositoryTests(TestDbContext db) => _db = db;
+        private readonly Mock<IEmployeeRepository> _employeeRepositoryMock;
+
+        public EmployeeRepositoryTests()
+        {
+            _employeeRepositoryMock = new Mock<IEmployeeRepository>();
+
+            var employeeMock = new Mock<DbSet<Employee>>();
+
+            var mockContext = new Mock<TestDbContext>();
+
+            mockContext.Setup(m => m.Employees).Returns(employeeMock.Object);
+        }
 
         [Fact]
         public void GetAllEmployees_ShouldRetrieveAllEmployeesFromRepository()
         {
-            var mockEmployeeRepo = new Mock<IEmployeeRepository>();
-            var expectedEmployees = new List<EmployeeResponse>
+            var options = new DbContextOptionsBuilder<TestDbContext>()
+                .UseSqlServer("Data Source=DESKTOP-90OC7A4\\SQLEXPRESS;Initial Catalog=Test_db;User Id=sa;Password=4121;MultipleActiveResultSets=True;TrustServerCertificate=True").Options;
+
+            using (var _db = new TestDbContext(options))
             {
-                new EmployeeResponse { Id = 1, FullName = "Employee A", Mobile = "09151234567" },
-                new EmployeeResponse { Id = 2, FullName = "Employee B", Mobile = "09151234568" }
-            };
-            mockEmployeeRepo.Setup(repo => repo.GetAll()).Returns(expectedEmployees.ToList());
-            var employeeRepository = new EmployeeRepository(_db);
+                var _lst = _db.Employees.ToList();
 
-            var actualEmployees = employeeRepository.GetAll();
+                ///////////////////////////////////////////////
 
-            Assert.NotNull(actualEmployees);
-            Assert.Equal(expectedEmployees.Count, actualEmployees.Count);
+                var expectedEmployees = new List<EmployeeResponse>
+                {
+                    new EmployeeResponse { Id = 1, FullName = "Employee A", Mobile = "09151234567" },
+                    new EmployeeResponse { Id = 2, FullName = "Employee B", Mobile = "09151234568" }
+                };
+                _employeeRepositoryMock.Setup(repo => repo.GetAll()).Returns(expectedEmployees);
+                var employeeRepository = new EmployeeRepository(_db);
+
+                var actualEmployees = employeeRepository.GetAll();
+
+                Assert.NotNull(actualEmployees);
+                Assert.Equal(expectedEmployees.Count, actualEmployees.Count);
+            }
         }
     }
 }
